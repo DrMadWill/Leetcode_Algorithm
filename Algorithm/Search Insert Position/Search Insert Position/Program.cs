c630abe5bb27b3f2cc886f1d5ac57698b5092bdc
DrMadWill <nofelsalahov9@gmail.com>
2022-06-19 10:45:46 +0400

    Algo Search Insert Position Complated
    Algo 35
    
    Seed:https://leetcode.com/problems/search-insert-position/

---
+++ Algorithm/Search Insert Position/Search Insert Position/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_Insert_Position
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 5, 6 };
+            int target = 7  ;
+            Console.WriteLine(SearchInsert(nums, target));
+
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+
+            foreach (int item in nums)
+            {
+                if (item == target)
+                {
+                    return Array.IndexOf(nums, item);
+                }
+            }
+
+            return -1;
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int index = Search(nums, target);
+            if (index < 0)
+            {
+                foreach (int item in nums)
+                {
+                    if (item > target)
+                    {
+                        index = Array.IndexOf(nums, item);
+                        break;
+                    }
+                }
+
+                if (index < 0)
+                {
+                    index = nums.Length;
+                }
+
+            }
+            return index;
+
+
+        }
+    }
+}

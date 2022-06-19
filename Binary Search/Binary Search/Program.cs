5081109be883af0f09ca52931780222547a20fdd
DrMadWill <nofelsalahov9@gmail.com>
2022-06-19 10:37:16 +0400

    main: Binary Search Complate
    LeetCode Algo 704
    
    seed:https://leetcode.com/problems/binary-search/

---
+++ Binary Search/Binary Search/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    internal class Program
+    {
+
+        //https://leetcode.com/problems/binary-search/
+        static void Main(string[] args)
+        {
+            var nums = new int[] { -1, 0, 3, 5, 9, 12 };
+            var target = 9;
+            Console.WriteLine(Search(nums,target));
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+            foreach (int item in nums)
+            {
+                if (item == target)
+                {
+                    return Array.IndexOf(nums, item);
+                }
+            }
+            return -1;
+        }
+    }
+}
